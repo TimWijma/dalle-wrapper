b85c95eb11fc71944970f1c12a5a5b1a4ea15359
Tim Wijma <tim.wijma@gmail.com>
2023-12-26 23:01:25 +0100

    very basic image creating

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,37 @@
 <script lang="ts">
     import OpenAi from "openai";
 
-    const openai = new OpenAi();
+    let apiKey = localStorage.getItem("openai-api-key") || "";
+
+    const openai = new OpenAi({
+        apiKey,
+        dangerouslyAllowBrowser: true,
+    });
+
+    let image = "";
 
     const createImage = async () => {
-        const image = await openai.images.generate({
+        const response = await openai.images.generate({
             model: "dall-e-2",
-            prompt: "Create an image of a white cat",
+            prompt: "A bowl of soup that is also a portal to another dimension, digital art",
             size: "256x256",
 			response_format: "b64_json"
         });
 
-		console.log(image);
+		console.log(response);
 		
-		console.log(image.data);
-		
+		console.log(response.data);
+
+        image = response.data[0].b64_json || "";
     };
 </script>
 
-hello
+<input bind:value={apiKey} on:input={() => {
+    openai.apiKey = apiKey
+    localStorage.setItem("openai-api-key", apiKey)
+}} />
 
 <button on:click={createImage}>Create Image</button>
+
+<!-- svelte-ignore a11y-img-redundant-alt -->
+<img src="data:image/png;base64,{image}" alt="DALL-E Generated image" />
