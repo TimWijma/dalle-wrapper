c62e4cae1f11aba6a950557ec115b3ca0e7fbc10
Tim Wijma <tim.wijma@gmail.com>
2023-12-26 22:36:15 +0100

    remove vite env

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
+    import OpenAi from "openai";
 
+    const openai = new OpenAi();
+
+    const createImage = async () => {
+        const image = await openai.images.generate({
+            model: "dall-e-2",
+            prompt: "Create an image of a white cat",
+            size: "256x256",
+			response_format: "b64_json"
+        });
+
+		console.log(image);
+		
+		console.log(image.data);
+		
+    };
 </script>
 
 hello
+
+<button on:click={createImage}>Create Image</button>
